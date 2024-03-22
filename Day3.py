ea9e8a13633a8acaa7d0db26f4e322eba3737504
KQPH <82293933+KQPH@users.noreply.github.com>
2024-03-21 22:48:08 -0700

    Day 3 Part 2 solved

--- Day3.py
+++ Day3.py
@@ -1,7 +1,7 @@
 #Part 1
 import re
 
-all_recipes: set = set()
+original_recipes: set = set()
 wanted_recipes: set = set()
 wanted_recipes_buffer: set = set()
 ingredients: set = set()
@@ -11,7 +11,7 @@ find_ingredients = re.compile(r'(?:\=|\+) ([^\n+]+)')
 
 with open("Day3Input.txt", "r", encoding = "utf-8") as f:
   recipe = f.readline()[8:-1].split(', ')[0]
-  all_recipes.add(recipe)
+  original_recipes.add(recipe)
   wanted_recipes.add(recipe)
 
 # While we still have any wanted_recipes
@@ -41,6 +41,54 @@ while len(wanted_recipes) >= 1:
     wanted_recipes.add(recipe)
   wanted_recipes_buffer.clear()
 
-print(all_recipes)
+print(original_recipes)
+print(ingredients)
+print(len(ingredients))
+
+print("Part 2")
+
+# Part 2
+
+original_recipes: set = set()
+wanted_recipes: set = set()
+wanted_recipes_buffer: set = set()
+ingredients: set = set()
+
+with open("Day3Input.txt", "r", encoding = "utf-8") as f:
+  recipes = f.readline()[8:-1].split(', ')
+  for recipe in recipes:
+    original_recipes.add(recipe)
+    wanted_recipes.add(recipe)
+
+# While we still have any wanted_recipes
+while len(wanted_recipes) >= 1:
+  # Iterate through all wanted_recipes
+  for wanted_recipe in wanted_recipes:
+    # For each wanted_recipe, iterate through each line of the input
+    with open("Day3Input.txt", "r", encoding = "utf-8") as f:
+      for line in f.readlines():
+        recipe = re.findall(find_recipe, line)
+        # If recipe is empty, continue to the next line
+        if not recipe:
+          continue
+        # If the current recipe is a wanted_recipe
+        if recipe[0] in wanted_recipes:
+          # PART 2 DIFFERENCE: remove the current recipe if it is present
+          ingredients.discard(recipe[0])
+          # Read the rest of the line to find all ingredients used
+          ingredients_used = map(str.strip,re.findall(find_ingredients, line))
+          # For each ingredient on the line
+          for ingredient in ingredients_used:
+            # Add the ingredient as a wanted recipe and to the ingredient list
+            if ingredient not in ingredients:
+              wanted_recipes_buffer.add(ingredient)
+              ingredients.add(ingredient)
+  # After iterating through the entire wanted_recipes, move the ones in the buffer into this main one
+  wanted_recipes.clear()
+  for recipe in wanted_recipes_buffer:
+    wanted_recipes.add(recipe)
+  wanted_recipes_buffer.clear()
+
+print(original_recipes)
 print(ingredients)
 print(len(ingredients))
